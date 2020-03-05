b0370fbe3a1d8e9845df24380d82f2157137fae9
Joseph Chamochumbi <sephxd1234@gmail.com>
2020-03-05 22:13:39 +0100

    feat: configure and start server

---
+++ backend/src/index.js
@@ -0,0 +1,18 @@
+require("dotenv").config({ path: "variables.env" });
+
+const createServer = require("./createServer");
+const db = require("./db");
+
+const server = createServer();
+
+server.start(
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL
+    }
+  },
+  ({ port }) => {
+    console.log(`Server running at: http:\/\/localhost:${port}`);
+  }
+);
