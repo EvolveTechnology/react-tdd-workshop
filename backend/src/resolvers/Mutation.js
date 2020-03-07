7be6e48581b25fbd35f7434791cdc440d53dbfa9
Joseph Chamochumbi <sephxd1234@gmail.com>
2020-03-07 08:30:41 +0100

    feat: update seen status

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -8,6 +8,21 @@ const Mutation = {
     );
 
     return contribution;
+  },
+  async updateSeen(parent, args, ctx, info) {
+    const { id } = args;
+    const { seen, ...rest } = await ctx.db.query.contribution({
+      where: { id }
+    });
+
+    if (seen) {
+      return { ...rest, id, seen };
+    }
+
+    return ctx.db.mutation.updateContribution({
+      data: { seen: true },
+      where: { id }
+    }, info);
   }
 };
 
--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -2,6 +2,10 @@ const Query = {
   async getAllContributions(parent, args, ctx, info) {
     const contributions = await ctx.db.query.contributions();
     return contributions;
+  },
+  async getContribution(parent, args, ctx, info) {
+    const contribution = await ctx.db.query.contribution(args);
+    return contribution;
   }
 };
 
