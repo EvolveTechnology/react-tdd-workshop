9f57b2120cbb79f43eb09763d3097f04a4cb64ed
Joseph Chamochumbi <sephxd1234@gmail.com>
2020-03-07 20:57:00 +0100

    feat: contribute more

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -9,6 +9,17 @@ const Mutation = {
 
     return contribution;
   },
+  async contributeMore(parent, args, ctx, info) {
+    const { id, qty } = args;
+    const contribution = await ctx.db.query.contribution({
+      where: { id }
+    });
+
+    return ctx.db.mutation.updateContribution({
+      data: { qty: contribution.qty + qty },
+      where: { id }
+    });
+  },
   async updateSeen(parent, args, ctx, info) {
     const { id } = args;
     const { seen, ...rest } = await ctx.db.query.contribution({
@@ -19,10 +30,13 @@ const Mutation = {
       return { ...rest, id, seen };
     }
 
-    return ctx.db.mutation.updateContribution({
-      data: { seen: true },
-      where: { id }
-    }, info);
+    return ctx.db.mutation.updateContribution(
+      {
+        data: { seen: true },
+        where: { id }
+      },
+      info
+    );
   }
 };
 
