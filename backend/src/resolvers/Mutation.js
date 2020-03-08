23c8849ae65acecc08af1f8e33f54110d30ed0c6
Joseph Chamochumbi <sephxd1234@gmail.com>
2020-03-08 09:24:04 +0100

    feat: create and resolve signIn mutation

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -64,6 +64,29 @@ const Mutation = {
     });
 
     return user;
+  },
+  async signIn(parent, args, ctx, info) {
+    const { email, password } = args;
+    const user = await ctx.query.user({ where: { email } });
+
+    if (!user) {
+      throw new Error(`User not found, ${email}`);
+    }
+
+    const valid = await bcrypt.compare(password, user.password);
+
+    if (!valid) {
+      throw new Error("Invalid password");
+    }
+
+    const token = jw.sign({ userId: user.id }, process.env.APP_SECRET);
+
+    ctx.response.cookie("token", token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365
+    });
+
+    return true;
   }
 };
 
