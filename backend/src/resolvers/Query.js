774142845e6ee1e1c1527fa1ab1056849861fb4f
Joseph Chamochumbi <sephxd1234@gmail.com>
2020-03-10 16:24:54 +0100

    feat: get my contributions

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -7,6 +7,33 @@ const Query = {
     const contribution = await ctx.db.query.contribution(args, info);
     return contribution;
   },
+  async getMyContributions(parent, args, ctx, info) {
+    const { userId } = ctx.request;
+
+    if (!userId) {
+      throw new Error("Must be logged in to get your contributions");
+    }
+
+    return ctx.db.query.contributions(
+      {
+        where: {
+          user: {
+            id: userId
+          }
+        }
+      },
+      `{ 
+		id
+		createdAt 
+		updatedAt
+		qty 
+		message
+		seen
+		private
+		user { id name } 
+	}`
+    );
+  },
   whoAmI(parent, args, ctx, info) {
     if (!ctx.request.userId) {
       return null;
