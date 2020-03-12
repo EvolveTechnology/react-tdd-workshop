003146c54de889192b36567949c8819ae4d937cb
Joseph Chamochumbi <sephxd1234@gmail.com>
2020-03-12 21:19:05 +0100

    test: problem - credit to icon providers

--- frontend/src/__tests__/footer.test.js
+++ frontend/src/__tests__/footer.test.js
@@ -1,33 +1,20 @@
 import React from "react";
 
-import { Elements } from "@stripe/react-stripe-js";
-import { loadStripe } from "@stripe/stripe-js";
-
 import App from "App";
 
 import { render } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 
-beforeAll(() => {
-  jest.mock("@stripe/stripe-js", () => ({
-    loadStripe: () => ({ createPaymentMethod: () => {} })
-  }));
-});
-
 describe("Credit icon providers", () => {
-  const stripePromise = loadStripe();
-  const { getByText } = render(
-    <Elements stripe={stripePromise}>
-      <App />
-    </Elements>
-  );
-  const element = getByText(/icons8/i);
-
   it("Mentions the icon providers", () => {
-    expect(element).toBeInTheDocument();
+    expect(true).toBe(false);
   });
 
   it("Links to the icon providers", () => {
-    expect(element.getAttribute("href")).toMatch("icons8.com");
+    expect(true).toBe(false);
   });
+});
+
+test("Credit style", () => {
+  expect(true).toBe(false);
 });
