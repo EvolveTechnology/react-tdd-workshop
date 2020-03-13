2bc429db3e224bfaf7855ad6a2f96e801510226a
Joseph Chamochumbi <sephxd1234@gmail.com>
2020-03-13 17:33:33 +0100

    test: mock stripe using promises and placeholder for mocks - no act

--- frontend/src/__mocks__/@stripe/stripe-js.js
+++ frontend/src/__mocks__/@stripe/stripe-js.js
@@ -1,8 +1,21 @@
 import * as stripeLib from "@stripe/stripe-js";
 
+const noop = () => {};
+
 stripeLib.loadStripe = (...args) =>
-  Promise.resolve(null).then(maybeStripe =>
-    maybeStripe ? maybeStripe(...args) : null
-  );
+  Promise.resolve((...values) => {
+    const [
+      elements,
+      createToken,
+      createPaymentMethod,
+      confirmCardPayment
+    ] = values.slice(1);
+    return {
+      elements: elements || noop,
+      createToken: createToken || noop,
+      createPaymentMethod: createPaymentMethod || noop,
+      confirmCardPayment: confirmCardPayment || noop
+    };
+  }).then(maybeStripe => (maybeStripe ? maybeStripe(...args) : null));
 
 module.exports = stripeLib;
