d2e1d94bf6545c37da5856dae0bbdb5823aab3a8
Joseph Chamochumbi <sephxd1234@gmail.com>
2020-03-13 17:08:39 +0100

    test: mock stripe api with null object

--- frontend/src/__mocks__/@stripe/stripe-js.js
+++ frontend/src/__mocks__/@stripe/stripe-js.js
@@ -1,5 +1,8 @@
 import * as stripeLib from "@stripe/stripe-js";
 
-stripeLib.loadStripe = () => ({ createPaymentMethod: () => {} });
+stripeLib.loadStripe = (...args) =>
+  Promise.resolve(null).then(maybeStripe =>
+    maybeStripe ? maybeStripe(...args) : null
+  );
 
 module.exports = stripeLib;
