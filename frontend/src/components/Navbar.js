fa0c321fa4f89f065ce7aedcccfeec37f9cd52a2
Joseph Chamochumbi <sephxd1234@gmail.com>
2020-03-14 23:20:34 +0100

    test: partial solution, but loginBtn should not be present in login route

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -13,11 +13,11 @@ export function Navbar() {
       data-testid="navbar"
     >
       <Text p={2} fontWeight="bold" fontSize={4}>
-        <NavLink to="/">Buy me a coffee</NavLink>
+        <NavLink to="/" data-testid="home">Buy me a coffee</NavLink>
       </Text>
       <Box mx="auto" />
       <Text variant="nav" fontSize={3} fontWeight="bold">
-        <NavLink to="/login">Log In</NavLink>
+        <NavLink to="/login" data-testid="login">Log In</NavLink>
       </Text>
     </Flex>
   );
