b1991abe61477d457a2bc31066ac3b8f27ddba50
Joseph Chamochumbi <sephxd1234@gmail.com>
2020-03-14 22:13:52 +0100

    feat: bring in the Apollo Client, with all providers in route.js

--- frontend/src/containers/Landing.js
+++ frontend/src/containers/Landing.js
@@ -1,18 +1,35 @@
 import React from "react";
 import { Flex, Heading } from "rebass/styled-components";
 
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+
 import { Dialog } from "components/Dialog";
 import { ContributionCard } from "components/ContributionCard";
 import { Header } from "components/Header";
 
 import "@reach/dialog/styles.css";
 
+const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY);
+const stripeOptions = {
+  fonts: [
+    {
+      family: "Inconsolata",
+      src:
+        "url('https://fonts.gstatic.com/s/inconsolata/v18/QldKNThLqRwH-OJ1UHjlKGlZ5qg.woff2') format('woff2')",
+      style: "normal"
+    }
+  ]
+};
+
 function Landing() {
   return (
     <>
       <Flex px={2} py={4} alignItems="center" flexDirection="column">
         <Header />
-        <Dialog />
+        <Elements stripe={stripePromise} options={stripeOptions}>
+          <Dialog />
+        </Elements>
       </Flex>
 
       <Flex px={1} my={2} justifyContent="center">
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,20 +1,34 @@
 import React from "react";
 import { Route, Switch, BrowserRouter } from "react-router-dom";
+
+import { ApolloProvider } from "@apollo/react-hooks";
+import { ThemeProvider } from "styled-components";
+
+import theme from "styles/theme";
+import GlobalStyle from "styles/global";
+
 import { Container } from "components/Container";
+import { Footer } from "components/Footer";
 import { Navbar } from "components/Navbar";
 
+import client from "client";
+
 import Landing from "containers/Landing";
-import { Footer } from "components/Footer";
 
 const Routes = () => (
   <BrowserRouter>
-    <Container>
-      <Navbar />
-      <Switch>
-        <Route exact path="/" component={Landing} />
-      </Switch>
-      <Footer />
-    </Container>
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Container>
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={Landing} />
+          </Switch>
+          <Footer />
+        </Container>
+      </ThemeProvider>
+    </ApolloProvider>
   </BrowserRouter>
 );
 
