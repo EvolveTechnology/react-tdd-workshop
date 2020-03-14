089761a70e5f32607aaed635ce85352d76e08700
Joseph Chamochumbi <sephxd1234@gmail.com>
2020-03-14 22:13:52 +0100

    feat: bring in the Apollo Client, with all providers in route.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,38 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { ThemeProvider } from "styled-components";
-import { Elements } from "@stripe/react-stripe-js";
-import { loadStripe } from "@stripe/stripe-js";
-
-import theme from "styles/theme";
-import GlobalStyle from "styles/global";
 
 import * as serviceWorker from "./serviceWorker";
 import Routes from "routes";
 
-const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY);
-
-ReactDOM.render(
-  <Elements
-    stripe={stripePromise}
-    options={{
-      fonts: [
-        {
-          family: "Inconsolata",
-          src:
-            "url('https://fonts.gstatic.com/s/inconsolata/v18/QldKNThLqRwH-OJ1UHjlKGlZ5qg.woff2') format('woff2')",
-          style: "normal"
-        }
-      ]
-    }}
-  >
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Routes />
-    </ThemeProvider>
-  </Elements>,
-  document.getElementById("root")
-);
+ReactDOM.render(<Routes />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
