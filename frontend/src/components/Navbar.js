d04c0a86be383e535d61e6968dc5aa60bf63588a
Joseph Chamochumbi <sephxd1234@gmail.com>
2020-03-19 11:23:24 +0100

    feat: implement logout mutation on navigation bar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,11 +1,22 @@
 import React from "react";
-import { NavLink, useLocation } from "react-router-dom";
-import { Box, Flex, Text } from "rebass/styled-components";
+import { NavLink, useLocation, useHistory } from "react-router-dom";
+import { Box, Button, Flex, Text } from "rebass/styled-components";
+import { useMutation } from "@apollo/react-hooks";
+import { useIdentity } from "providers/Auth";
+import { SIGN_OUT } from "graphql/mutations";
+import { IDENTITY } from "graphql/queries";
 
 export function Navbar() {
   const { pathname } = useLocation();
   const isLogin = pathname.includes("login");
   const isSignUp = pathname.includes("signup");
+  const { id, loading: authLoading } = useIdentity();
+
+  const history = useHistory();
+  const [logOut] = useMutation(SIGN_OUT, {
+    onCompleted: () => history.push("/"),
+    refetchQueries: [{ query: IDENTITY }]
+  });
 
   return (
     <Flex
@@ -22,19 +33,50 @@ export function Navbar() {
         </NavLink>
       </Text>
       <Box mx="auto" />
-      {!isLogin && (
-        <Text variant="nav" fontSize={3} fontWeight="bold" mx={2}>
-          <NavLink to="/login" data-testid="login">
-            Log In
-          </NavLink>
-        </Text>
-      )}
-      {!isSignUp && (
-        <Text variant="nav" fontSize={3} fontWeight="bold" mx={2}>
-          <NavLink to="/signup" data-testid="signup">
-            Sign Up
-          </NavLink>
-        </Text>
+
+      {!authLoading && (
+        <>
+          {!id && !isLogin && (
+            <Text
+              variant="nav"
+              fontSize={3}
+              fontWeight="bold"
+              mx={2}
+              style={{ textDecoration: "underline" }}
+            >
+              <NavLink to="/login" data-testid="login">
+                Log In
+              </NavLink>
+            </Text>
+          )}
+          {!id && !isSignUp && (
+            <Text
+              variant="nav"
+              fontSize={3}
+              fontWeight="bold"
+              mx={2}
+              style={{ textDecoration: "underline" }}
+            >
+              <NavLink to="/signup" data-testid="signup">
+                Sign Up
+              </NavLink>
+            </Text>
+          )}
+          {id && (
+            <Button
+              color="secondary"
+              variant="outline"
+              fontSize={3}
+              fontWeight="bold"
+              mx={2}
+              onClick={logOut}
+              tyle={{ border: "1px solid" }}
+              data-testid="logout"
+            >
+              Logout
+            </Button>
+          )}
+        </>
       )}
     </Flex>
   );
